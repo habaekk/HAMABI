a406de956f835d80f65911aed16dfb4067898b6c
habaekk <haback1493@gmail.com>
2025-07-31 19:17:57 +0900

    chore(icon): edit name BackArow to BackArrowIcon.

--- components/BackArrowIcon.js
+++ components/BackArrowIcon.js
@@ -1,5 +1,5 @@
 // components/BackIcon.js
-export default function BackIcon() {
+export default function BackArrowIcon() {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
       <path fill="none" d="M0 0h24v24H0z" />
