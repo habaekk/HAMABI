70a9baa1f03c47be84c1a07653a17d501f7e834d
habaekk <haback1493@gmail.com>
2025-08-01 19:18:39 +0900

    chore(icons): remove role img. since it's not decided

--- components/UserIcon.js
+++ components/UserIcon.js
@@ -1,7 +1,7 @@
 // components/UserIcon.js
 export default function UserIcon() {
     return (
-      <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" width="24" height="24">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
         <circle cx="12" cy="8" r="4" fill="#2196F3" />
         <rect x="4" y="14" width="16" height="6" rx="3" fill="#2196F3" />
       </svg>
