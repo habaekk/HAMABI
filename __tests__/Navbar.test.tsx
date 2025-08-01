4dba2f6fdbbc67a9e1229e2c10bd8fa8b406f699
habaekk <haback1493@gmail.com>
2025-08-01 19:28:30 +0900

    test(navbar): add test when there is only a left icon

--- __tests__/Navbar.test.tsx
+++ __tests__/Navbar.test.tsx
@@ -16,13 +16,26 @@ describe('Navbar', () => {
   it('renders left and right icons if provided', () => {
     render(
         <Navbar
-        title="Icons"
-        left={<div data-testid="left-slot"><ArchiveIcon /></div>}
-        right={<div data-testid="right-slot"><UserIcon /></div>}
+            title="Icons"
+            left={<div data-testid="left-slot"><ArchiveIcon /></div>}
+            right={<div data-testid="right-slot"><UserIcon /></div>}
         />
     )
 
     expect(screen.getByTestId('left-slot')).toBeInTheDocument();
     expect(screen.getByTestId('right-slot')).toBeInTheDocument();
   });
+
+  it('renders left icon only when right is omitted', () => {
+    render(
+        <Navbar
+            title="Back Arrow"
+            left={<div data-testid="back-arrow"><BackArrowIcon /></div>}
+        />
+    )
+
+    expect(screen.getByTestId('back-arrow')).toBeInTheDocument();
+    expect(screen.queryByTestId('right-slot')).toBeNull();
+  })
+
 });
