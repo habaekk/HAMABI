2f1d8ba30de719410cfcac5507f5b65defb81572
habaekk <haback1493@gmail.com>
2025-08-02 19:25:20 +0900

    feat(RobotChatBox): add component and css

---
+++ components/layout/RobotChatBox.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './RobotChatBox.module.css';
+
+export const RobotBookBox = () => {
+    return (
+        <div className={styles.chatBox} />
+    );
+};
