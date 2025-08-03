9af76e846a71f05927183d1a31db376ad71501ce
habaekk <haback1493@gmail.com>
2025-08-03 12:31:19 +0900

    fix(robotChatBox): fix typo

--- components/layout/RobotChatBox.tsx
+++ components/layout/RobotChatBox.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './RobotChatBox.module.css';
 
-export const RobotBookBox = () => {
+export const RobotChatBox = () => {
     return (
         <div className={styles.chatBox} />
     );
