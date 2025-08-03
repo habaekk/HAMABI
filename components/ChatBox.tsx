c394543231bf2b0272140c57e31faa2a77391cfc
habaekk <haback1493@gmail.com>
2025-08-03 15:33:24 +0900

    feat(chatBox): add component and css

---
+++ components/ChatBox.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './ChatBox.module.css';
+
+type Props = {
+    text: string;
+    isUser: boolean;
+};
+
+export const ChatBox = ({ text, isUser }: Props) => {
+    const boxClass = [
+        styles.chatBox,
+        isUser ? styles.userChatBox : styles.robotChatBox
+    ].join(' ');
+
+    return (
+        <div className={boxClass}>
+            <p className={styles.chatText}>{text}</p>
+        </div>
+    );
+};
