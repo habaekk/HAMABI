c97af129d90d6d20fb388176e2bdc7010aaf7516
habaekk <haback1493@gmail.com>
2025-08-10 19:54:18 +0900

    style: apply tailwind to global css

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,15 +12,15 @@ export const metadata: Metadata = {
 
 const orbitron = Orbitron({ subsets: ["latin"], display: "swap" });
 
-export default function Layout({ children }) {
+export default function Layout({ children }: { children: React.ReactNode }): React.ReactElement {
   return (
     <html lang="en">
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
-      <body className={orbitron.className}>
-        <div className="app-container">
+      <body className={`${orbitron.className} bg-white text-neutral-900 dark:bg-neutral-900 dark:text-neutral-100`}>
+        <div className="min-h-screen">
           <main>{children}</main>
         </div>
       </body>
