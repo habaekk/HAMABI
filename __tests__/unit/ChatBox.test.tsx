113ba3154e50467463d4ddd2cc898f3469ed8b35
habaekk <haback1493@gmail.com>
2025-08-10 20:28:51 +0900

    fix: fix test error

--- __tests__/unit/ChatBox.test.tsx
+++ __tests__/unit/ChatBox.test.tsx
@@ -13,15 +13,19 @@ describe('ChatBox', () => {
   it('applies robot style when isUser is false', () => {
     render(<ChatBox text="robot message" isUser={false} />);
     const box = screen.getByTestId('chat-box');
-    expect(box.className).toMatch(/robotChatBox/);
-    expect(box.className).not.toMatch(/userChatBox/);
+    expect(box.className).toMatch(/bg-\[#FFB18C\]/);
+    expect(box.className).toMatch(/rounded-br-2xl/);
+    expect(box.className).toMatch(/rounded-bl-0/);
+    expect(box.className).not.toMatch(/bg-\[#D9D9D9\]/);
   });
 
   it('applies user style when isUser is true', () => {
     render(<ChatBox text="user message" isUser={true} />);
     const box = screen.getByTestId('chat-box');
-    expect(box.className).toMatch(/userChatBox/);
-    expect(box.className).not.toMatch(/robotChatBox/);
+    expect(box.className).toMatch(/bg-\[#D9D9D9\]/);
+    expect(box.className).toMatch(/rounded-bl-2xl/);
+    expect(box.className).toMatch(/rounded-br-0/);
+    expect(box.className).not.toMatch(/bg-\[#FFB18C\]/);
   });
 });
 
