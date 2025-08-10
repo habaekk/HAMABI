9984d17a31124e9ca3290ac6f9b6580c3cae56bb
habaekk <haback1493@gmail.com>
2025-08-10 18:15:45 +0900

    fix: fix lint error

--- components/ui/UserAvatar/UserAvatar.stories.tsx
+++ components/ui/UserAvatar/UserAvatar.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/nextjs';
 import UserAvatar from './index';
 
 const meta: Meta<typeof UserAvatar> = {
--- components/ui/UserDetailCard/UserDetailCard.stories.tsx
+++ components/ui/UserDetailCard/UserDetailCard.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/nextjs';
 import UserDetailCard from './index';
 
 const meta: Meta<typeof UserDetailCard> = {
